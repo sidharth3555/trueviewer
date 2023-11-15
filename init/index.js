ce3df8a3162cc4402e1c7957688e1662382fb65f
sidharth2003 <86048378+Hvhjjjbghjjjbbbbvvvvvv@users.noreply.github.com>
2023-11-15 19:06:36 +0530

    error page not found and empty request body handeled

--- init/index.js
+++ init/index.js
@@ -2,7 +2,11 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const listing = require("../models/listing.js");
-
+// ../ used to move out of the init folder and
+// go to models folder if you write ./ then
+// it will find inside current folder that is init folder
+//only aap.js can use ./ to require bcz its already outside of all
+//folders 
 const MONGO_URL = "mongodb://127.0.0.1:27017/sidbnb";
 
 main()
---
+++ utils/expresserror.js
@@ -0,0 +1,12 @@
+class ExpressError extends Error{
+//Error is the universal error class
+constructor(statuscode,message){
+    super();//to invoke the parent class constructor i.e error class
+    //and that will return errorcode and message according to passing argument
+this.statuscode=statuscode;
+this.message=message;
+}
+
+}
+
+module.exports=ExpressError;
